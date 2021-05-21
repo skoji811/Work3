657e9d6307b13d4d5d68692def64dd9e8e1101cd
skoji811 <s.koji811@gmail.com>
2021-05-21 17:54:22 +0900

    first commit（修正）

---
+++ zzz09/src/main/java/com/example/demo/Zzz09Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zzz09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zzz09Application.class, args);
+	}
+
+}
